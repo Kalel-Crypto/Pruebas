0e00f82919174bc5e20b9c277b54a9ffa13f4928
Kalel <master.creeperfavorite@gmail.com>
2026-09-11 22:22:09 -0700

    Se agrega un segundo metodo

--- test_calculadora.py
+++ test_calculadora.py
@@ -3,4 +3,12 @@ from calculadora import Calculadora
 def test_add():
     calc = Calculadora()
     assert calc.add(2, 3) == 5
+
+def test_imprimir(capsys):
+    calc = Calculadora()
+    calc.imprimir("Hola, mundo!")
+    captured = capsys.readouterr()
+    assert captured.out == "Hola, mundo!\n"
+
+
     
